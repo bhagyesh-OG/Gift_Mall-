5d041074f0dbc30e771bc55fcc6326adff39ee1d
bhagyesh-OG <bhagyesh.a.chaudhari@gmail.com>
2026-08-18 15:19:24 +0530

    feat: load cinematic visual layer

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import "./cinematic.css";
 
 const basename = import.meta.env.BASE_URL === "/" ? undefined : import.meta.env.BASE_URL.replace(/\/$/, "");
 
